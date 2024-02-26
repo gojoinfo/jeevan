314e3e88c090dd3061dc9e9ae46dfdf1aaa2d5cc
Jeevan <Jeevan@gmail.com>
2024-02-26 14:35:41 +0530

    e

--- name.java
+++ name.java
@@ -2,7 +2,9 @@ public class name
 {
 	public static void main (String[] args)
 	{
-		system.out.println("Jai shree ram")
+		system.out.println("Jai shree ram");
+                system.out.println("Namaste");
+
 	}
 }
 
