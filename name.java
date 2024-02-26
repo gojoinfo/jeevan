34a19b48257fe6c4f7ec84ff65016634e03b0af2
gojoinfo <159603391+gojoinfo@users.noreply.github.com>
2024-02-26 14:55:34 +0530

    Update name.java

--- name.java
+++ name.java
@@ -3,6 +3,7 @@ public class name
 	public static void main (String[] args)
 	{
 		system.out.println("Jai shree ram");
+		system.out.println("har har mhadev");
                 system.out.println("Namaste");
 
 	}
